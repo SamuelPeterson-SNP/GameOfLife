a4f0b860e61bd5b0809bfc9f0fa3fcc8f638f4af
Samuel Peterson <zama201bc@users.noreply.github.com>
2015-11-01 15:13:06 +0100

    Update Cell.java

--- src/board/Cell.java
+++ src/board/Cell.java
@@ -6,10 +6,7 @@ public class Cell {
 	/**
 	 * Cell Object.  Specifically instantiated by AliveCell and DeadCell in this particular Project.
 	 * Contains a Rules.
-	 * 
-	 * Design Flaw:  Cell should not have a location field.  Will correct that in next version.  Location of
-	 * Cell should be managed solely in the Board Field.
-	 */
+	
 	
 	Rules rule = new Rules();
 	
